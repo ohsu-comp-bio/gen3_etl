e9cedfddaa2aba37961d14d52b1b6898aad3bee6
Brian Walsh <walsbr@ohsu.edu>
2021-06-01 19:43:27 -0700

    Adds ndjson handler

--- gen3_etl/utils/ioutils.py
+++ gen3_etl/utils/ioutils.py
@@ -12,16 +12,21 @@ import json
 
 
 class JsonReader:
+    """Read json and return dict iterator."""
+
     def __init__(self, path):
+        """Open file."""
         if path.endswith(".json.gz"):
             self.fp = io.TextIOWrapper(io.BufferedReader(gzip.GzipFile(path)))
         else:
             self.fp = open(path, "r", encoding='utf-8')
 
     def __iter__ (self):
+        """Return self."""
         return self
 
     def __next__(self):
+        """Iterate to next row."""
         try:
             l = self.fp.readline()
             if len(l) < 1:
@@ -55,6 +60,8 @@ def reader(path, **kwargs):
     elif path.endswith(".tsv"):
         return csv.DictReader(open(path, "r", encoding='utf-8'), delimiter="\t", **kwargs)
     elif path.endswith(".json"):
+        return JsonReader(path)
+    elif path.endswith(".ndjson"):
         return JsonReader(path)
     else:
         return open(path, "r", encoding='utf-8')
